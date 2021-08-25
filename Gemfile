d53e5bd556a35b138aaa1a7e823e43e7bf8adeed
Shaqri <peter200970@hotmail.com>
2021-08-25 05:49:06 -0500

    Create seed database using FFaker gem

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ ruby '3.0.1'
 # dotenv
 gem 'dotenv-rails', groups: [:development, :test]
 
+#aws
+gem "aws-sdk-s3", require: false
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
 # Use postgresql as the database for Active Record
@@ -38,6 +41,7 @@ group :development do
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'ffaker'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'ffaker'
+
+user = User.new(name: FFaker::Name.first_name, email: FFaker::Internet.email, password: 'victor5', password_confirmation: 'victor5');
+
+if user.save
+  10.times do
+    post = Post.create(
+      title: FFaker::Name.first_name,
+      description: FFaker::Lorem.paragraph,
+      location: FFaker::Address.country,
+      user_id: user.id,
+    )
+    post.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/example.jpg')), filename: 'detection.jpg')
+  end
+end
