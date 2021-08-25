62940cb4bfcfedb9d108d6e55eebc4833f8755ba
Shaqri <peter200970@hotmail.com>
2021-08-25 06:37:14 -0500

    Add index action to posts controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::FavoritesController < ApiController
 
   def index
       @favorite_posts = current_user.favorite_posts
-      render json: PostSerializer.new(@favorite_posts);
+      render json: PostSerializer.new(@favorite_posts).serializable_hash.to_json, status: 200
   end
 
   private
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::PostsController < ApiController
-    before_action :set_post
+    before_action :set_post, except: [:index]
+    skip_before_action :authenticate_token!
+
+    def index
+        @posts = Post.all
+        render json: PostSerializer.new(@posts).serializable_hash.to_json, status: 200
+    end
 
     def show 
         if @post.present?
