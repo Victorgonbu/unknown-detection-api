31be1e1d0264dea1ee44113c9522185349088efd
Shaqri <peter200970@hotmail.com>
2021-08-27 04:01:24 -0500

    Modify index action in posts to handle search params and create search scope in Posts model

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,9 +2,19 @@ class Api::V1::PostsController < ApiController
     before_action :set_post, except: [:index]
 
     def index
-        @posts = Post.all.includes(:favorites, image_attachment: :blob)
-        render json: PostSerializer.new(@posts, { params: 
+        if params[:search]
+            query = params[:search]
+            @posts = Post.search(query)
+            if @posts.any?
+                render json: PostSerializer.new(@posts).serializable_hash.to_json, status: 200
+            else
+                render json: { errors: ["No matches found"] }, status: 404
+            end
+        else
+            @posts = Post.all.includes(:favorites, image_attachment: :blob)
+            render json: PostSerializer.new(@posts, { params: 
             { user_favorites: serlializer_params } }).serializable_hash.to_json, status: 200
+        end  
     end
 
     def show 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
   has_one_attached :image
   has_many :favorites, dependent: :destroy
   belongs_to :author, class_name: "User", foreign_key: "user_id"
+
+  scope :search, -> (query) { where('lower(title) LIKE ?', "%#{query}%") }
 end
